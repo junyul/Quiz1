f1c938480530e22bd928fa2810de407a128b4480
longjunyu <longjunyu@wifi-roaming-128-4-124-0.host.udel.edu>
2017-09-13 17:03:44 -0400

    。

--- src/QB/Score.java
+++ src/QB/Score.java
@@ -2,8 +2,9 @@ package QB;
 import java.util.Scanner;
 public class Score{
 Scanner input = new Scanner(System.in);
+	private double Players_score;
 
-	System.out.println("Please enter your ATT : ");
+	System.out.println("Please enter your ATT ;");
 	A = input.nextInt();
 	System.out.println("Please enter your COMP : ");
 	C = input.nextInt();
@@ -12,9 +13,18 @@ Scanner input = new Scanner(System.in);
 	System.out.println("Please enter your TD : ");
 	T = input.nextInt();
 	System.out.println("Please enter your INT : ");
-	tn = input.nextInt();
-	public Sc{
+	I = input.nextInt();
+
+	
+	public Score(int C, int A, int Y, int T, int I){
+		
+		double Players_score= (((C/A-0.3)*5+(Y/A-3)*0.25+(T/A)*20+(2.375-25*I/A))/6)*100;
+	}
+	
+	public double QB() {
+		return Players_score;
+	}
+		
 		
 	
-	}
 }
