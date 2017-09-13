6d266f403669805ecf9b1f27fa459ee6ce9d0653
longjunyu <longjunyu@wifi-roaming-128-4-124-0.host.udel.edu>
2017-09-13 17:02:41 -0400

    👌

---
+++ src/QB/Score.java
@@ -0,0 +1,20 @@
+package QB;
+import java.util.Scanner;
+public class Score{
+Scanner input = new Scanner(System.in);
+
+	System.out.println("Please enter your ATT : ");
+	A = input.nextInt();
+	System.out.println("Please enter your COMP : ");
+	C = input.nextInt();
+	System.out.println("Please enter your YDS : ");
+	Y = input.nextInt();
+	System.out.println("Please enter your TD : ");
+	T = input.nextInt();
+	System.out.println("Please enter your INT : ");
+	tn = input.nextInt();
+	public Sc{
+		
+	
+	}
+}
